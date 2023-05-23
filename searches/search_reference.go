778adf67afafbc0102773d49795396f151b4e697
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2023-05-23 22:19:57 +0100

    Added searches -> GetSearchReference

---
+++ searches/search_reference.go
@@ -0,0 +1,40 @@
+package searches
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/api/iterator"
+)
+
+func GetSearchReference(ctx context.Context, reference string, vrm string) (sref string, err error) {
+
+	client, err := firestore.NewClient(ctx, "transfer-360")
+
+	if err != nil {
+		return sref, fmt.Errorf("error creating firestore client: %w", err)
+	}
+
+	defer client.Close()
+
+	doci := client.Collection("searches").Where("your_reference", "==", reference).Where("vrm", "==", vrm).Documents(ctx)
+
+	for {
+		doc, err := doci.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Warnln(err)
+		} else {
+
+			sref = doc.Ref.ID
+			break
+
+		}
+
+	}
+
+	return sref, nil
+}
