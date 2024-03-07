575f84ffee9a2683f0dad7153d1242f40949f0b1
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2024-03-07 16:24:51 +0000

    Address Line validation Added

--- validate/address.go
+++ validate/address.go
@@ -114,3 +114,78 @@ func AddressValidation(address []string, postCode string) (Address, error) {
 		}
 	}
 }
+
+func AddressLineValidation(address string, postCode string) (Address, error) {
+
+	addr := Address{}
+
+	if len(os.Getenv("MAP_KEY")) == 0 {
+		return addr, ErrMissingAddressKey
+	}
+
+	addressURL := strings.ReplaceAll(address, " ", "%20")
+
+	mapsResponse, err := http.Get(fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?key=%s&address=%s", os.Getenv("MAP_KEY"), addressURL))
+
+	if err != nil {
+		log.Errorln("AddressLineValidation:", err)
+		return addr, fmt.Errorf("%w %v", ErrUnexpected, err)
+	} else {
+		defer mapsResponse.Body.Close()
+
+		mapInfo := data{}
+
+		err = json.NewDecoder(mapsResponse.Body).Decode(&mapInfo)
+
+		if err != nil {
+			log.Errorln("AddressLineValidation:", err)
+			return addr, fmt.Errorf("%w %v", ErrUnexpected, err)
+		} else {
+
+			if mapInfo.Status == "OK" {
+
+				for _, ar := range mapInfo.Results[0].AddressComponents {
+					for _, art := range ar.Types {
+
+						switch art {
+						case "street_number":
+							addr.AddressLine1 = ar.LongName
+							break
+						case "route":
+							addr.AddressLine1 = strings.TrimSpace(fmt.Sprintf("%s %s", addr.AddressLine1, ar.LongName))
+							break
+						case "locality":
+							addr.AddressLine2 = ar.LongName
+							break
+						case "postal_town":
+							addr.City = ar.LongName
+							break
+						case "administrative_area_level_2":
+							if len(addr.City) == 0 {
+								addr.City = ar.LongName
+							}
+							break
+						case "country":
+							addr.Country = ar.LongName
+							break
+						case "postal_code":
+							addr.Postcode = ar.LongName
+							break
+						}
+
+					}
+				}
+
+				// 28.11 - Override post code due to part post code being returned.
+				if len(postCode) > 0 {
+					addr.Postcode = postCode
+				}
+
+				return addr, nil
+
+			} else {
+				return addr, fmt.Errorf("%w %v", ErrInvalidMapResults, err)
+			}
+		}
+	}
+}
