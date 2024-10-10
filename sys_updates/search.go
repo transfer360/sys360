813839f77277cf39663efea732a7c7b85ce1d945
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2024-10-10 18:18:19 +0100

    Add new fields to SearchData struct
    Introduce PercentSuccess, IgnoreSearchPartners, and SearchErrors fields to the SearchData struct. These additions will enhance the detail and control over search results and error handling.

--- sys_updates/search.go
+++ sys_updates/search.go
@@ -9,13 +9,16 @@ import (
 /* send result of search */
 
 type SearchData struct {
-	Sref          string `json:"sref"`
-	ClientID      string `json:"client_id"`
-	IsHireVehicle bool   `json:"is_hire_vehicle"`
-	FleetID       int    `json:"fleet_id"`
-	Reference     string `json:"reference"`
-	VRM           string `json:"vrm"`
-	ScanDateTime  string `json:"scan_date_time"`
+	Sref                 string   `json:"sref"`
+	ClientID             string   `json:"client_id"`
+	IsHireVehicle        bool     `json:"is_hire_vehicle"`
+	FleetID              int      `json:"fleet_id"`
+	Reference            string   `json:"reference"`
+	VRM                  string   `json:"vrm"`
+	ScanDateTime         string   `json:"scan_date_time"`
+	PercentSuccess       float64  `json:"percent_success"`
+	IgnoreSearchPartners []string `json:"ignore_search_partners"`
+	SearchErrors         []string `json:"search_errors"`
 }
 
 func (sd *SearchData) Update(ctx context.Context, source string) error {
